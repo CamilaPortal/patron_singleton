00ad6be6383996be640efa150dbff1cec5a7b59c
CamilaPortal <c.portal@alumno.um.edu.ar>
2024-10-24 14:35:27 -0300

    Patron singleton

---
+++ patronSingleton.py
@@ -0,0 +1,15 @@
+class InstitutoEducativo:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self):
+        self.nombre = "Instituto Educativo"
+
+instituto1 = InstitutoEducativo()
+instituto2 = InstitutoEducativo()
+
+print(instituto1 is instituto2)
